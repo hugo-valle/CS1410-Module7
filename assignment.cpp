f864fdf88ded7300679f5490731b0ae9346cb8da
hugo-valle <hugovalle1@weber.edu>
2017-10-10 12:10:53 -0600

    Adding another Constructor for initial value.

--- assignment.cpp
+++ assignment.cpp
@@ -10,7 +10,9 @@ private:
     unsigned int count;         // count
 
 public:
-    Counter():count(0)          // constructor
+    Counter():count(0)          // constructor no args
+    {};
+    Counter(int c):count(c)    // constructor with one arg
     {};
 
     unsigned int get_count()    // returns count
@@ -23,9 +25,7 @@ public:
     Counter operator ++()          // increment prefix
     {
         ++count;
-        Counter temp;
-        temp.count = count;
-        return temp;
+        return Counter(count);
     }
 };
 // Prototypes
